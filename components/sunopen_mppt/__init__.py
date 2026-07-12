3ab11b5fcda295ea91f74aa4e0a014e0231a473f
shenyuzi8 <133303086+shenyuzi8@users.noreply.github.com>
2026-07-12 19:36:04 +0800

    Update __init__.py

--- components/sunopen_mppt/__init__.py
+++ components/sunopen_mppt/__init__.py
@@ -1,19 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client, modbus
+from esphome.components import ble_client
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["ble_client"]
-AUTO_LOAD = ["modbus", "sensor", "number", "switch", "text_sensor"]
+AUTO_LOAD = ["modbus", "modbus_controller", "sensor", "switch", "number", "text_sensor"]
 
 sunopen_mppt_ns = cg.esphome_ns.namespace("sunopen_mppt")
 SunopenMPPT = sunopen_mppt_ns.class_(
-    "SunopenMPPT", 
-    ble_client.BLEClientNode, 
+    "SunopenMPPT",
+    ble_client.BLEClientNode,
     cg.Component
 )
-
-CONF_SUNOPEN_MPPT_ID = "sunopen_mppt_id"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SunopenMPPT),
