322a0b7f780e2919c4df785e8e43bcf2112a3aac
shenyuzi8 <133303086+shenyuzi8@users.noreply.github.com>
2026-07-12 19:37:32 +0800

    Update __init__.py

--- components/sunopen_mppt/__init__.py
+++ components/sunopen_mppt/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import ble_client
 from esphome.const import CONF_ID
+from esphome import automation
 
 DEPENDENCIES = ["ble_client"]
 AUTO_LOAD = ["modbus", "modbus_controller", "sensor", "switch", "number", "text_sensor"]
@@ -13,11 +14,17 @@ SunopenMPPT = sunopen_mppt_ns.class_(
     cg.Component
 )
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SunopenMPPT),
-}).extend(ble_client.BLE_CLIENT_SCHEMA)
+CONF_SUNOPEN_MPPT_ID = "sunopen_mppt_id"
+
+# 关键：使用 ensure_list 允许列表格式
+CONFIG_SCHEMA = cv.ensure_list(
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(SunopenMPPT),
+    }).extend(ble_client.BLE_CLIENT_SCHEMA)
+)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await ble_client.register_ble_node(var, config)
+    for conf in config:
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await ble_client.register_ble_node(var, conf)
