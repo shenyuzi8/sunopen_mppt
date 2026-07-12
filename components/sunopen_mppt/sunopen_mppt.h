ebd0a5600fa60314c7448da8cceae921085dc85e
shenyuzi8 <133303086+shenyuzi8@users.noreply.github.com>
2026-07-12 19:59:02 +0800

    Update sunopen_mppt.h

--- components/sunopen_mppt/sunopen_mppt.h
+++ components/sunopen_mppt/sunopen_mppt.h
@@ -7,29 +7,59 @@
 namespace esphome {
 namespace sunopen_mppt {
 
+static const uint16_t SERVICE_UUID = 0xFFE0;
+static const uint16_t TX_CHAR_UUID = 0xFFE1;
+static const uint16_t RX_CHAR_UUID = 0xFFE2;
+
+class SunopenMPPTDevice;
+
 class SunopenMPPT : public ble_client::BLEClientNode, public Component {
  public:
   void setup() override;
   void loop() override;
+  void dump_config() override;
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                            esp_ble_gattc_cb_param_t *param) override;
 
   void set_throttle(uint32_t throttle) { this->throttle_ = throttle; }
+  void register_device(SunopenMPPTDevice *device) { this->devices_.push_back(device); }
+  
+  void write_command(const std::vector<uint8_t> &data);
+  float get_setup_priority() const override;
 
  protected:
   void enable_notify_();
   void poll_data_();
-  void write_modbus_command_(const std::vector<uint8_t> &data);
+  bool parse_modbus_byte_(uint8_t byte);
+  void process_data_(const std::vector<uint8_t> &data);
 
   uint16_t tx_handle_{0};
   uint16_t rx_handle_{0};
   bool ready_{false};
   uint32_t last_poll_{0};
   uint32_t throttle_{5000};
+  
+  std::vector<uint8_t> rx_buffer_;
+  uint32_t last_modbus_byte_{0};
+  static constexpr uint32_t RX_TIMEOUT = 100;
+  
+  std::vector<SunopenMPPTDevice *> devices_;
+};
 
-  static constexpr uint16_t SERVICE_UUID = 0xFFE0;
-  static constexpr uint16_t TX_CHAR_UUID = 0xFFE1;
-  static constexpr uint16_t RX_CHAR_UUID = 0xFFE2;
+class SunopenMPPTDevice {
+ public:
+  void set_parent(SunopenMPPT *parent) { this->parent_ = parent; }
+  void set_address(uint8_t address) { this->address_ = address; }
+  virtual void on_modbus_data(const std::vector<uint8_t> &data) = 0;
+  
+  void write_command(const std::vector<uint8_t> &data) { 
+    this->parent_->write_command(data); 
+  }
+
+ protected:
+  friend SunopenMPPT;
+  SunopenMPPT *parent_{nullptr};
+  uint8_t address_{0x01};
 };
 
 }  // namespace sunopen_mppt
